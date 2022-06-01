04b347578454d7a600960e3c4f3f923830aeb0b5
aelbanyard <64694151+aelbanyard@users.noreply.github.com>
2022-06-01 16:04:21 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Alert from "react-bootstrap/Alert";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <div>Home Github Pages</div>;
 }
 
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div>About</div>;
 }
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/" className="navlink">Home</Link>
-        <Link to="/about" className="navlink">About</Link>
+        <Link to="/" className="navlink">
+          Home
+        </Link>
+        <Link to="/about" className="navlink">
+          About
+        </Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="about" element={<About/>} />
-        <Route path="*" element={
-          <Alert variant={'danger'}>There's nothing here!</Alert>
-        } />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route
+          path="*"
+          element={<Alert variant={"danger"}>There's nothing here!</Alert>}
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
