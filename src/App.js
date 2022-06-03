49a433c541228018da5492c76629b361d5742c8e
aelbanyard <64694151+aelbanyard@users.noreply.github.com>
2022-06-03 23:49:11 +0200

    Added Function Todos

--- src/App.js
+++ src/App.js
@@ -11,6 +11,29 @@ function About() {
   return <div>About</div>;
 }
 
+function Todos() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
+  }, []);
+  return (
+    // JSX
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <div>
+          {todo.id +
+            " - " +
+            todo.title +
+            (todo.complete ? " (done) " : " (open)")}
+        </div>
+      ))}
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
@@ -21,10 +44,14 @@ function App() {
         <Link to="/about" className="navlink">
           About
         </Link>
+        <Link to="/todos" className="navlink">
+          ToDos
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="todos" element={<Todos />} />
         <Route
           path="*"
           element={<Alert variant={"danger"}>There's nothing here!</Alert>}
